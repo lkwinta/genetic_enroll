d4b48fd40a5f5c210cbdbc8f06d2e731435094a9
Łuaksz Kwinta <lukaszmind@gmail.com>
2025-05-31 18:44:33 +0200

    settings interface

--- enroll_web/src/app/components/Settings/interfaces/AlgorithmSettings.tsx
+++ enroll_web/src/app/components/Settings/interfaces/AlgorithmSettings.tsx
@@ -1,3 +1,38 @@
+type MutationType = 'A' | 'B';
+type CrossoverType = 'A' | 'B'; 
+
+interface AlgorithmSettings {
+    mutationType: MutationType;
+    crossoverType: CrossoverType;
+
+    generationsCount: number;
+    populationSize: number;
+
+    earlyStopping: boolean;
+
+    elitismRate: number;
+    crossoverRate: number;
+    mutationRate: number;
+}
+
+interface FitnessFunctionSettings {
+    preferenceWeight: number;
+    capacityWeight: number;
+    diversityWeight: number;
+    penaltyWeight: number;
+}
+
+interface PerformanceSettings {
+    enableParallelProcessing: boolean;
+    threadCount: number;
+}
+
+interface CombinedSettings {
+    algorithm: AlgorithmSettings;
+    fitnessFunction: FitnessFunctionSettings;
+    performance: PerformanceSettings;
+};
+
 export default interface GASettings {
   populationSize: number;
   generations: number;
