61799edc9589aec897e044a066acdc24a002ec66
MateuszMaciaszczyk <maciaszczyk@student.agh.edu.pl>
2025-06-02 11:44:54 +0200

    rever last commit

--- enroll_web/src/app/components/Settings/Settings.tsx
+++ enroll_web/src/app/components/Settings/Settings.tsx
@@ -15,10 +15,8 @@ import PerformanceSettingsSection from './PerformanceSettingsSection';
 import './styles/settings.css';
 import { AlgorithmSettingsState, FitnessFunctionSettingsState, PerformanceSettingsState } from './interfaces/AlgorithmSettings';
 import { FilesContext } from '@/app/global_state';
-import {useRouter} from "next/navigation";
 
 const Settings: React.FC = () => {
-    const router = useRouter();
     const defaultAlgorithmSettings: AlgorithmSettingsState = {
         mutationType: "A",
         crossoverType: "B",
@@ -54,88 +52,44 @@ const Settings: React.FC = () => {
     const [fitnessFunctionSettings, setFitnessFunctionSettings] = useState<FitnessFunctionSettingsState>(defaultFitnessFunctionSettings);
     const [performanceSettings, setPerformanceSettings] = useState<PerformanceSettingsState>(defaultPerformanceSettings);
     const {scheduleFile, preferencesFile} = useContext(FilesContext);
-    
+
     const runAlgorithm = async () => {
         if (!scheduleFile || !preferencesFile) {
             console.error("Schedule file or preferences file is not set");
             return;
         }
 
+
         setIsRunning(true);
+        const settings = {
+            algorithmSettings,
+            fitnessFunctionSettings,
+            performanceSettings
+        };
 
-        try {
-            const settings = {
-                algorithmSettings,
-                fitnessFunctionSettings,
-                performanceSettings
-            };
+        const response = await fetch("http://127.0.0.1:5000/settings", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(settings),
+        });
 
-            const settingsResponse = await fetch("http://127.0.0.1:5000/settings", {
+        if (response.ok) {
+            const data = new FormData();
+            data.append("file", scheduleFile.file);
+
+            const res = await fetch("http://127.0.0.1:5000/upload/schedule", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(settings),
+                body: data
             });
 
-            if (!settingsResponse.ok) {
-                console.error("Failed to save settings");
-                setIsRunning(false);
-                return;
-            }
-
-            const scheduleData = new FormData();
-            scheduleData.append("file", scheduleFile.file);
-
-            const scheduleResponse = await fetch("http://127.0.0.1:5000/upload/schedule", {
-                method: "POST",
-                body: scheduleData,
-            });
-
-            if (!scheduleResponse.ok) {
-                console.error("Failed to upload schedule file");
-                setIsRunning(false);
-                return;
-            }
-
-            const preferencesData = new FormData();
-            preferencesData.append("file", preferencesFile.file);
-
-            const preferencesResponse = await fetch("http://127.0.0.1:5000/upload/preferences", {
-                method: "POST",
-                body: preferencesData,
-            });
-
-            if (!preferencesResponse.ok) {
-                console.error("Failed to upload preferences file");
-                setIsRunning(false);
-                return;
-            }
-
-            const resultsResponse = await fetch("http://127.0.0.1:5000/evolve", {
-                method: "GET",
-            });
-
-            if (resultsResponse.ok) {
-                const blob = await resultsResponse.blob();
-                const url = window.URL.createObjectURL(blob);
-                const a = document.createElement("a");
-                a.style.display = "none";
-                a.href = url;
-                a.download = "results.csv";
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-
-                router.push("/pages/results");
-            } else {
-                console.error("Failed to run algorithm");
-            }
-        } catch (error) {
-            console.error("An error occurred:", error);
-        } finally {
-            setIsRunning(false);
+            console.log(res);
+        } else {
+            console.error("Failed to save settings");
         }
+
+        setIsRunning(false);
     };
 
     const resetSettings = () => {
