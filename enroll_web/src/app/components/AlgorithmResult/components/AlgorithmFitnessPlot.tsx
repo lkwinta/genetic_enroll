059785fb85972fb0ad2be705858e2aed65de228b
Łuaksz Kwinta <lukaszmind@gmail.com>
2025-06-09 20:55:40 +0200

    change plot library

--- enroll_web/src/app/components/AlgorithmResult/components/AlgorithmFitnessPlot.tsx
+++ enroll_web/src/app/components/AlgorithmResult/components/AlgorithmFitnessPlot.tsx
@@ -1,6 +1,5 @@
-import dynamic from 'next/dynamic';
 import React, { useEffect, useRef } from 'react';
-const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Label } from 'recharts';
 
 export type FitnessHistory = number[];
 
@@ -18,44 +17,51 @@ const AlgorithmFitnessPlot: React.FC<AlgorithmFitnessPlotProps> = ({ fitnessHist
     const lineColor = isDark.current ? 'rgb(96, 165, 250)' : 'rgb(59, 130, 246)';
     const textColor = isDark.current ? 'rgb(255, 255, 255)' : 'rgb(31, 41, 55)';
     const gridColor = isDark.current ? 'rgba(255, 255, 255, 0.2)' : 'rgba(0, 0, 0, 0.1)';
+    const tooltipStyle: React.CSSProperties = {
+        color: isDark.current ? 'rgb(255, 255, 255)' : 'rgb(31, 41, 55)',
+        backgroundColor: isDark.current ? 'rgba(10, 10, 10, 0.5)' : 'rgba(255, 255, 255, 0.8)',
+        borderColor: isDark.current ? 'rgba(180, 180, 180, 0.5)' : 'rgba(0, 0, 0, 0.1)',
+        borderRadius: '8px',
+    };
 
     return (
         <div className="bg-gray-100 dark:bg-gray-700 rounded-lg p-4 h-100 border border-gray-200 dark:border-gray-600">
             {fitnessHistory && fitnessHistory.length > 0 ? (
-                <div className="w-full h-full">
-                    <Plot
-                        data={[
-                            {
-                                x: fitnessHistory.map((_, index) => index),
-                                y: fitnessHistory,
-                                type: 'scatter',
-                                mode: 'lines',
-                                line: { color: lineColor },
-                                name: 'Fitness'
-                            }
-                        ]}
-                        layout={{
-                            title: { text: 'Fitness History', font: { color: textColor } },
-                            xaxis: {
-                                title: { text: 'Epoch', font: { color: textColor } },
-                                color: textColor,
-                                gridcolor: gridColor
-                            },
-                            yaxis: {
-                                title: { text: 'Fitness', font: { color: textColor } },
-                                color: textColor,
-                                gridcolor: gridColor,
-                                range: [0, 1],
-                                dtick: 0.2
-                            },
-                            margin: { l: 50, r: 30, t: 40, b: 40 },
-                            paper_bgcolor: 'rgba(0,0,0,0)',
-                            plot_bgcolor: 'rgba(0,0,0,0)',
-                        }}
-                        style={{ width: '100%', height: '100%' }}
-                        config={{ responsive: true, staticPlot: true }}
-                    />
-                </div>
+                <ResponsiveContainer width={'100%'} height={'100%'}>
+                    <LineChart data={fitnessHistory.map((fitness, index) => ({ epoch: index + 1, fitness }))} margin={{ top: 12, right: 12, bottom: 12 }}>
+                        <Line
+                            type="monotone"
+                            dataKey="fitness"
+                            stroke={lineColor}
+                            strokeWidth={2}
+                            dot={false}/>
+                        <XAxis 
+                            dataKey="epoch" 
+                            stroke={textColor} 
+                            type="number"
+                        >
+                            <Label 
+                                value="Epoch"
+                                offset={-10}
+                                fill={textColor}
+                                style={{ fontSize: '14px' }}
+                                position={"insideBottom"}
+                            />
+                        </XAxis>
+                        <YAxis stroke={textColor} domain={[0, 1]}>
+                            <Label 
+                                value="Fitness"
+                                angle={-90}
+                                offset={10}
+                                fill={textColor}
+                                style={{ fontSize: '14px' }}
+                                position={"insideLeft"}
+                            />
+                        </YAxis>
+                        <CartesianGrid stroke={gridColor} />
+                        <Tooltip contentStyle={tooltipStyle}/>
+                    </LineChart>
+                </ResponsiveContainer>
             ) : (
                 <div className="flex items-center justify-center h-full text-center text-gray-500 dark:text-gray-400">
                     <div>
