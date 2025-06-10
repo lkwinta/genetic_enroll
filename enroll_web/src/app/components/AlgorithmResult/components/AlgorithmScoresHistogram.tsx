3a3cfda1cf19016855d2310f4319c2c27cdf1c9b
Łuaksz Kwinta <lukaszmind@gmail.com>
2025-06-10 13:40:28 +0200

    add histogram

--- enroll_web/src/app/components/AlgorithmResult/components/AlgorithmScoresHistogram.tsx
+++ enroll_web/src/app/components/AlgorithmResult/components/AlgorithmScoresHistogram.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Label, Legend, Area } from 'recharts';
+import SettingsSliderInput from '../../Settings/components/SettingsSliderInput';
+
+import '@/app/components/Settings/styles/settings.css';
 
 export type StudentScore = {
     student: string;
@@ -13,15 +16,17 @@ export interface AlgorithmScoresHistogramProps {
 
 const AlgorithmScoresHistogram: React.FC<AlgorithmScoresHistogramProps> = ({ scores }) => {
     const isDark = useRef<boolean>(false);
+    const [binWidth, setBinWidth] = useState<number>(2);
 
     useEffect(() => {
         isDark.current = window.matchMedia("(prefers-color-scheme: dark)").matches;
     });
 
-    const binWidth = 2;
     const scoresHistogram: Record<number, number> = {};
+    let maxWidth = 1;
 
     if (scores && scores.length > 0) {
+        maxWidth = Math.max(...scores!.map(s => s.score));
         scores.forEach(({ score }) => {
             const bin = Math.floor(score / binWidth) * binWidth;
             scoresHistogram[bin] = (scoresHistogram[bin] || 0) + 1;
@@ -33,7 +38,7 @@ const AlgorithmScoresHistogram: React.FC<AlgorithmScoresHistogramProps> = ({ sco
         count: count
     }));
 
-    const barColor = isDark.current ? 'rgb(151, 191, 191)' : 'rgb(64, 245, 245)';
+    const barColor = isDark.current ? 'rgb(151, 191, 191)' : 'rgb(46, 179, 179)';
     const textColor = isDark.current ? 'rgb(255, 255, 255)' : 'rgb(31, 41, 55)';
     const gridColor = isDark.current ? 'rgba(255, 255, 255, 0.2)' : 'rgba(0, 0, 0, 0.1)';
     const tooltipStyle: React.CSSProperties = {
@@ -44,27 +49,28 @@ const AlgorithmScoresHistogram: React.FC<AlgorithmScoresHistogramProps> = ({ sco
     };
 
     return (
-        <div className="bg-gray-100 dark:bg-gray-700 rounded-lg p-4 pt-10 h-110 border border-gray-200 dark:border-gray-600">
+        <>
+            <SettingsSliderInput 
+                label="Bin Width"
+                value={binWidth} 
+                onChange={(value) => {setBinWidth(value)}}
+                min={1}
+                max={maxWidth}
+                step={1}
+            />
+             <div className="bg-gray-100 dark:bg-gray-700 rounded-lg p-4 pt-10 h-110 border border-gray-200 dark:border-gray-600">
+            
             {scores && scores.length > 0 ? (
                 <ResponsiveContainer width={'100%'} height={'100%'}>
                     <ComposedChart data={scoresArray} margin={{ right: 12, bottom: 12 }}>
-                        {/* <Bar 
+                        <Bar 
                             dataKey={"count"}
                             fill={barColor}
-                        /> */}
-                        <Area
-                            dataKey={"count"}
-                            fill={barColor}
-                            stroke={barColor}
-                            strokeWidth={2}
-                            type="monotone"
-                            dot={false}
                         />
                         <XAxis 
                             dataKey="score" 
                             stroke={textColor} 
-                            type="category"
-                        >
+                            type="category">
                             <Label 
                                 value="Scores"
                                 offset={-10}
@@ -96,6 +102,7 @@ const AlgorithmScoresHistogram: React.FC<AlgorithmScoresHistogramProps> = ({ sco
                 </div>
             )}
         </div>
+        </>
     );
 }
 
